853e5b26d0486df39568de341a4695281ab5dea1
cdbarbosa <cdbarbosa@atlassian.com>
2020-03-26 20:13:30 -0300

    ajustando texto

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -65,7 +65,7 @@ export default function Details() {
 
       <View style={styles.contactBox}>
         <Text style={styles.heroTitle}>Salve o dia!</Text>
-        <Text style={styles.heroTitle}>Seja o herói desse caso.</Text>
+        <Text style={styles.heroTitle}>Seja o herói deste caso.</Text>
 
         <Text style={styles.heroDescription}>Entre em contato</Text>
 
